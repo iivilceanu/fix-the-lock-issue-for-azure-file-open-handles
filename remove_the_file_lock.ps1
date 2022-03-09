9859d76b649bf91c919d5bb66f5463d7db758a60
ILIE VILCEANU <71116646+iivilceanu@users.noreply.github.com>
2022-03-09 19:08:26 +0200

    Update and rename script to remove_the_file_lock.ps1

---
+++ remove_the_file_lock.ps1
@@ -0,0 +1,14 @@
+Connect-AzAccount
+#if running from Azure Powershell please run cmdlet 'Connect-AzAccount -UseDeviceAuthentication'
+#Set Azure Subscription
+Select-AzSubscription -subscriptionid "yoursubscriptionid"
+
+#create azure storage account context
+$Context = New-AzStorageContext -StorageAccountName "yourstorageaccount" -StorageAccountKey "yourstorageaccountkey"
+
+Get-AzStorageFileHandle -Context $Context -ShareName "filesharename" -Recursive | Sort-Object ClientIP,OpenTime
+
+#Set Context
+$Context = New-AzStorageContext -StorageAccountName "yourstorageaccount" -StorageAccountKey "yourstorageaccountkey"
+#To close the open handles
+Close-AzStorageFileHandle -Context $Context -ShareName "filesharename" -Path 'azcopy.exe' -CloseAll
